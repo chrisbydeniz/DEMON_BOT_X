a0c32de4f91066e2a70163a68ce5404a98096873
chrisbydeniz <nourelyakineazoul1@gmail.com>
2026-04-09 21:44:56 +0200

    settings.js

--- settings.js
+++ settings.js
@@ -1,1 +1,74 @@
-const a0_0x24ecee=a0_0x1901;function a0_0x1246(){const _0x3141e9=['1048Demmim','212505JzNvHp','143wqeTgH','428856HIxnWx','216811hASwdf','52aHGYst','270uErruZ','exports','9BkpzyU','72taYNod','👑\x20𝙎𝙃𝘼𝙍𝙀\x20𝘼𝙉𝘿\x20⚡️𝙎𝙐𝙋𝙋𝙊𝙍𝙏\x20❤️:-\x20𝐃𝐄𝐌𝐎𝐍\x20𝐄𝐘𝐄....!!','218812vpIdgC','401900pvQAIS','Lucifer....!!‎','311215xUwuWJ','10gdxCtm','Knight\x20Bot','919339344963','3.0.4'];a0_0x1246=function(){return _0x3141e9;};return a0_0x1246();}(function(_0x29dffb,_0x4ae9a0){const _0x40f515=a0_0x1901,_0x4d8c6e=_0x29dffb();while(!![]){try{const _0x5cc884=parseInt(_0x40f515(0x86))/0x1+-parseInt(_0x40f515(0x84))/0x2*(parseInt(_0x40f515(0x93))/0x3)+parseInt(_0x40f515(0x90))/0x4*(parseInt(_0x40f515(0x8c))/0x5)+-parseInt(_0x40f515(0x94))/0x6*(parseInt(_0x40f515(0x8f))/0x7)+-parseInt(_0x40f515(0x8b))/0x8*(-parseInt(_0x40f515(0x91))/0x9)+-parseInt(_0x40f515(0x87))/0xa*(-parseInt(_0x40f515(0x96))/0xb)+-parseInt(_0x40f515(0x8e))/0xc*(-parseInt(_0x40f515(0x8d))/0xd);if(_0x5cc884===_0x4ae9a0)break;else _0x4d8c6e['push'](_0x4d8c6e['shift']());}catch(_0x5f1b6d){_0x4d8c6e['push'](_0x4d8c6e['shift']());}}}(a0_0x1246,0x4abde));function a0_0x1901(_0x916893,_0x373d27){const _0x124674=a0_0x1246();return a0_0x1901=function(_0x19010b,_0x5cd92d){_0x19010b=_0x19010b-0x84;let _0xe89c01=_0x124674[_0x19010b];return _0xe89c01;},a0_0x1901(_0x916893,_0x373d27);}const settings={'packname':a0_0x24ecee(0x88),'author':a0_0x24ecee(0x85),'botName':'🔮\x20𝐃𝐞𝐦𝐨𝐧𝐁𝐨𝐭𝐱\x20🔥🤖','botOwner':'𝐋𝐮𝐜𝐢𝐟𝐞𝐫....!!🌈™','ownerNumber':a0_0x24ecee(0x89),'giphyApiKey':'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq','commandMode':'public','maxStoreMessages':0x14,'storeWriteInterval':0x2710,'description':a0_0x24ecee(0x95),'version':a0_0x24ecee(0x8a)};module[a0_0x24ecee(0x92)]=settings;
+const a0_0x24ecee = a0_0x1901;
+
+function a0_0x1246() {
+    const _0x3141e9 = [
+        '1048Demmim',
+        '212505JzNvHp',
+        '143wqeTgH',
+        '428856HIxnWx',
+        '216811hASwdf',
+        '52aHGYst',
+        '270uErruZ',
+        'exports',
+        '9BkpzyU',
+        '72taYNod',
+        '👑 00u43 byDeniz - SHARE AND SUPPORT ❤️',
+        '218812vpIdgC',
+        '401900pvQAIS',
+        '00u43 byDeniz....!!',
+        '311215xUwuWJ',
+        '10gdxCtm',
+        '00u43-XMD',
+        '33753191305',
+        '3.0.4'
+    ];
+    a0_0x1246 = function() {
+        return _0x3141e9;
+    };
+    return a0_0x1246();
+}
+
+(function(_0x29dffb, _0x4ae9a0) {
+    const _0x40f515 = a0_0x1901;
+    const _0x4d8c6e = _0x29dffb();
+    while (!![]) {
+        try {
+            const _0x5cc884 = parseInt(_0x40f515(0x86)) / 0x1 +
+                -parseInt(_0x40f515(0x84)) / 0x2 * (parseInt(_0x40f515(0x93)) / 0x3) +
+                parseInt(_0x40f515(0x90)) / 0x4 * (parseInt(_0x40f515(0x8c)) / 0x5) +
+                -parseInt(_0x40f515(0x94)) / 0x6 * (parseInt(_0x40f515(0x8f)) / 0x7) +
+                -parseInt(_0x40f515(0x8b)) / 0x8 * (-parseInt(_0x40f515(0x91)) / 0x9) +
+                -parseInt(_0x40f515(0x87)) / 0xa * (-parseInt(_0x40f515(0x96)) / 0xb) +
+                -parseInt(_0x40f515(0x8e)) / 0xc * (-parseInt(_0x40f515(0x8d)) / 0xd);
+            if (_0x5cc884 === _0x4ae9a0) break;
+            else _0x4d8c6e['push'](_0x4d8c6e['shift']());
+        } catch (_0x5f1b6d) {
+            _0x4d8c6e['push'](_0x4d8c6e['shift']());
+        }
+    }
+}(a0_0x1246, 0x4abde));
+
+function a0_0x1901(_0x916893, _0x373d27) {
+    const _0x124674 = a0_0x1246();
+    return a0_0x1901 = function(_0x19010b, _0x5cd92d) {
+        _0x19010b = _0x19010b - 0x84;
+        let _0xe89c01 = _0x124674[_0x19010b];
+        return _0xe89c01;
+    }, a0_0x1901(_0x916893, _0x373d27);
+}
+
+const settings = {
+    'packname': a0_0x24ecee(0x88),
+    'author': a0_0x24ecee(0x85),
+    'botName': '🔥 00u43 byDeniz 🤖',
+    'botOwner': '00u43 byDeniz....!!',
+    'ownerNumber': a0_0x24ecee(0x89),
+    'giphyApiKey': 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+    'commandMode': 'public',
+    'maxStoreMessages': 0x14,
+    'storeWriteInterval': 0x2710,
+    'description': a0_0x24ecee(0x95),
+    'version': a0_0x24ecee(0x8a)
+};
+
+module[a0_0x24ecee(0x92)] = settings;
